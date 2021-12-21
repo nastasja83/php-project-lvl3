971e7b7eabebb788ab9f60694c5c7e473a000f6a
Anastasiya Ostertag <nastasja83@mail.ru>
2021-12-21 22:14:08 +0700

    fix migration

--- database/migrations/2021_12_21_145813_create_urls_table.php
+++ database/migrations/2021_12_21_145813_create_urls_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Urls extends Migration
+class CreateUrlsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,7 +15,7 @@ class Urls extends Migration
     {
         Schema::create('urls', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->unique();
             $table->timestamps();
         });
     }

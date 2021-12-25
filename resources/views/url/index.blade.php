f83bad813fddf9434848f5111175e31e1ced47aa
Anastasiya Ostertag <nastasja83@mail.ru>
2021-12-25 17:28:10 +0700

    add index.blade.php

---
+++ resources/views/url/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+<main class="flex-grow-1">
+    <div class="container-lg">
+        <h1 class="mt-5 mb-3">{{ __('messages.Sites') }}</h1>
+        <div class="table-responsive">
+            <table class="table table-bordered table-hover text-nowrap">
+                <tr>
+                    <th>ID</th>
+                    <th>{{ __('messages.Name') }}</th>
+                    <th>{{ __('messages.Last check') }}</th>
+                    <th>{{ __('messages.Status code') }}</th>
+                </tr>
+                @if ($urls)
+                    @foreach ($urls as $url)
+                        <tr>
+                            <td>{{ $url->id }}</td>
+                            <td scope="row"><a href="{{ route('urls.show', ['url' => $url->id]) }}">{{ $url->name }}</a></td>
+                            <td>{{ $lastChecked[$url->id]->created_at ?? ''}}</td>
+                            <td>{{ $lastChecked[$url->id]->status_code ?? '' }}</td>
+                        </tr>
+                    @endforeach
+                @endif
+            </table>
+            <nav>
+                <ul class="pagination">
+                <li>{{ $urls->onEachSide(5)->links() }}</li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+</main>
+@endsection

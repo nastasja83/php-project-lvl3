7062b0cf283b7bcb9db17216d6cfd90df1138e7b
Anastasiya Ostertag <nastasja83@mail.ru>
2021-12-25 17:32:26 +0700

    add checking params in store

--- app/Http/Controllers/UrlCheckingController.php
+++ app/Http/Controllers/UrlCheckingController.php
@@ -2,27 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Http;
 use DiDom\Document;
 use Illuminate\Http\Client\HttpClientException;
-use GuzzleHttp\Exception\RequestException;
 
 class UrlCheckingController extends Controller
 {
-    public function store(Request $request, int $id)
+    public function store(int $id)
     {
         $url = DB::table('urls')->find($id);
         abort_unless($url, 404);
 
-        DB::table('url_checks')->insert([
-            'url_id' => $id,
-            'created_at' => Carbon::now()->toString(),
-            'updated_at' => Carbon::now()->toString()
-    ]);
+        try {
+            $response = Http::get($url->name);
+            $document = new Document($response->body());
+            $h1 = optional($document->first('h1'))->text();
+            $title = optional($document->first('title'))->text();
+            $description = optional($document->first('meta[name=description]'))->getAttribute('content');
 
-    flash(__('messages.Page has been checked successfully'))->success();
+            DB::table('url_checks')->insert([
+                    'url_id' => $id,
+                    'created_at' => Carbon::now()->toString(),
+                    'status_code' => $response->status(),
+                    'h1' => $h1,
+                    'title' => $title,
+                    'description' => $description,
+                    'updated_at' => Carbon::now()->toString()
+            ]);
+
+            flash(__('messages.Page has been checked successfully'))->success();
+        } catch (HttpClientException $exception) {
+            flash($exception->getMessage())->error();
+        }
+        return redirect()->route('urls.show', ['url' => $id]);
     }
 }

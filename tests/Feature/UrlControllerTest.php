5e6dad8f8a836e2bf2651bd4e1ec0cc6923b3f37
Anastasiya Ostertag <nastasja83@mail.ru>
2021-12-25 18:54:15 +0700

    add tests show, store, index

--- tests/Feature/UrlControllerTest.php
+++ tests/Feature/UrlControllerTest.php
@@ -2,21 +2,43 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
 class UrlControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     *
-     * @return void
-     */
-    public function test_example()
+    protected function setUp(): void
     {
-        $response = $this->get('/');
+        parent::setUp();
 
-        $response->assertStatus(200);
+        DB::table('urls')->insert([
+                'name' => 'https://example.ru',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+        ]);
+    }
+
+    public function testIndex()
+    {
+        $response = $this->get(route('urls.index'));
+        $response->assertOk();
+    }
+
+    public function testStore()
+    {
+        $data = ['url' => ['name' => 'https://example2.ru']];
+        $response = $this->post(route('urls.store'), $data);
+        $response->assertSessionHasNoErrors();
+        $response->assertRedirect();
+
+        $this->assertDatabaseHas('urls', $data['url']);
+    }
+
+    public function testShow()
+    {
+        $id = DB::table('urls')->insertGetId(['name' => 'https://example3.com']);
+        $response = $this->get(route('urls.show', ['url' => $id]));
+        $response->assertOk();
     }
 }

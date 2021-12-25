e4fc4fce8a0c96df0bc6f020a41e2eeee4743974
Anastasiya Ostertag <nastasja83@mail.ru>
2021-12-25 17:30:13 +0700

    add RefreshDatabase, DatabaseMigrations

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,15 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use RefreshDatabase;
+    use DatabaseTransactions;
+    use DatabaseMigrations;
 }

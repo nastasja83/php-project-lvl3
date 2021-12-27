ebf59180b6c580cd1a305fbec7cdf1f7265a577a
Anastasiya Ostertag <nastasja83@mail.ru>
2021-12-27 15:29:48 +0700

    change links for CSS/ Js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,8 @@
         <title>{{ __('messages.Page analyzer') }}</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="csrf-param" content="_token" />
-        <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-        <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+        <link href="/css/bootstrap.min.css" rel="stylesheet">
+        <script src="/js/bootstrap.bundle.min.js"></script>
     </head>
     <body class="min-vh-100 d-flex flex-column">
         <header class="flex-shrink-0">
